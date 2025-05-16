962078728611f52a4fc45a1072d3f645eee90632
Sephania96 <koutanyi@yahoo.com>
2025-05-16 03:16:20 +0100

    table function and insert data function created

--- python-generators-0x00/seed.py
+++ python-generators-0x00/seed.py
@@ -21,3 +21,31 @@ def connect_to_prodev():
         print(f"Connection Error: {err}")
         return None
 
+def create_table(connection):
+    cursor = connection.cursor()
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS user_data (
+            user_id VARCHAR(36) PRIMARY KEY,
+            name VARCHAR(255) NOT NULL,
+            email VARCHAR(255) NOT NULL,
+            age DECIMAL(5, 2) NOT NULL,
+            INDEX (user_id)
+        )
+    """)
+    print("Table user_data created successfully")
+    cursor.close()
+
+def insert_data(connection, filename):
+    cursor = connection.cursor()
+    with open(filename, newline='') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            try:
+                cursor.execute("""
+                    INSERT IGNORE INTO user_data (user_id, name, email, age)
+                    VALUES (%s, %s, %s, %s)
+                """, (str(uuid.uuid4()), row['name'], row['email'], row['age']))
+            except mysql.connector.Error as err:
+                print(f"Insert error: {err}")
+    connection.commit()
+    cursor.close()
