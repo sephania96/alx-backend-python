dedfa08383400687edbe91caeaffeeec4504cca3
Sephania96 <koutanyi@yahoo.com>
2025-05-28 14:22:48 +0100

    update

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -7,13 +7,11 @@ import unittest
 
 ##Test cases for access_nested_map function
 class TestAccessNestedMap(unittest.TestCase):
-    #Test cases for access_nested_map function
     @parameterized.expand([
         ("simple_path", {"a": 1}, ("a",), 1),
         ("nested_path", {"a": {"b": 2}}, ("a",), {"b": 2}),
         ("deep_path", {"a": {"b": 2}}, ("a", "b"), 2),
     ])
-    #Test cases for access_nested_map function
     def test_access_nested_map(self, name, nested_map, path, expected):
         self.assertEqual(access_nested_map(nested_map, path), expected)
 
