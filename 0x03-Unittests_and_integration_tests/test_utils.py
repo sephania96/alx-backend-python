863b453aac25d9c08a1cd0382eabb5e4e4c39358
Sephania96 <koutanyi@yahoo.com>
2025-05-28 13:59:07 +0100

    update

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -4,13 +4,14 @@
 import sys
 import os
 
+# Adjust path to import utils.py from the project root
 CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
 PROJECT_ROOT = os.path.abspath(os.path.join(CURRENT_DIR, ".."))
 sys.path.insert(0, PROJECT_ROOT)
 
-import unittest
-from parameterized import parameterized
 from utils import access_nested_map
+from parameterized import parameterized
+import unittest
 
 
 class TestAccessNestedMap(unittest.TestCase):
