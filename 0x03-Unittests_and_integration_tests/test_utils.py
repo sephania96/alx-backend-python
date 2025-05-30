4bc115445f2e08b96558ab75d54154feefc29deb
Sephania96 <koutanyi@yahoo.com>
2025-05-30 09:31:04 +0100

    update

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,20 +1,40 @@
 #!/usr/bin/env python3
+"""Unit test for utils.access_nested_map"""
 
-#Unit test for utils.access_nested_map
-from utils import access_nested_map
-from parameterized import parameterized
+import sys
+import os
+
+# Add the project root to the Python path to enable imports
+CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
+PROJECT_ROOT = os.path.abspath(os.path.join(CURRENT_DIR, ".."))
+sys.path.insert(0, PROJECT_ROOT)
+
 import unittest
+from parameterized import parameterized
+from utils import access_nested_map
 
-##Test cases for access_nested_map function
+
 class TestAccessNestedMap(unittest.TestCase):
+    """Test cases for access_nested_map function"""
+
     @parameterized.expand([
         ("simple_path", {"a": 1}, ("a",), 1),
         ("nested_path", {"a": {"b": 2}}, ("a",), {"b": 2}),
         ("deep_path", {"a": {"b": 2}}, ("a", "b"), 2),
     ])
     def test_access_nested_map(self, name, nested_map, path, expected):
+        """Test valid access of nested maps"""
         self.assertEqual(access_nested_map(nested_map, path), expected)
 
+    @parameterized.expand([
+        ("missing_key", {}, ("a",)),
+        ("missing_nested_key", {"a": 1}, ("a", "b")),
+    ])
+    def test_access_nested_map_exception(self, name, nested_map, path):
+        """Test KeyError is raised for missing keys in nested maps"""
+        with self.assertRaises(KeyError) as cm:
+            access_nested_map(nested_map, path)
+        self.assertEqual(str(cm.exception), repr(path[len(cm.exception.args[0]):][0]) if len(path) > 1 else repr(path[0]))
 
 if __name__ == '__main__':
     unittest.main()
