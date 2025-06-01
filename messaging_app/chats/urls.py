3637363ef6c8c0261e6f73fd3bfd1c4690430ea3
Sephania96 <koutanyi@yahoo.com>
2025-06-01 22:56:15 +0100

    update

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -2,7 +2,7 @@
 """URL routing for chat APIs."""
 
 from django.urls import path, include
-from rest_framework_nested import routers
+from rest_framework import routers
 from .views import ConversationViewSet, MessageViewSet
 
 router = routers.DefaultRouter()
--- messaging_app/messaging_app/urls.py
+++ messaging_app/messaging_app/urls.py
@@ -16,8 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework.routers import NestedDefaultRouter
-from rest_framework.routers import api-auth
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('chats.urls')),
