192daeabbe6c036842e88bfc494ec854b79bc840
Sephania96 <koutanyi@yahoo.com>
2025-06-08 21:53:20 +0100

    update

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -6,6 +6,7 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from .models import Conversation, Message
 from .serializers import ConversationSerializer, MessageSerializer
+from rest_framework.status import HTTP_403_FORBIDDEN
 
 class ConversationViewSet(viewsets.ModelViewSet):
     """ViewSet for handling conversations."""
