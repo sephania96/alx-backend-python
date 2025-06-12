033bc3a9443b2c773d212201ea14923e80aaa695
Sephania96 <koutanyi@yahoo.com>
2025-06-12 05:18:51 +0100

    update

--- Django-signals_orm-0x04/messaging/tests.py
+++ Django-signals_orm-0x04/messaging/tests.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 from django.contrib.auth.models import User
-from .models import Message, Notification
+from .models import Message, Notification, MessageHistory
 
 class NotificationSignalTestCase(TestCase):
     def setUp(self):
