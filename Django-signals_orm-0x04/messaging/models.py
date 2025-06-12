757fd423cf34eb623effecd4f27c7f5a47d6bc72
Sephania96 <koutanyi@yahoo.com>
2025-06-12 05:32:25 +0100

    parent thread

--- Django-signals_orm-0x04/messaging/models.py
+++ Django-signals_orm-0x04/messaging/models.py
@@ -11,6 +11,8 @@ class Message(models.Model):
     timestamp = models.DateTimeField(auto_now_add=True)
     edited = models.BooleanField(default=False)
     edited_by = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True, related_name='edited_messages')
+    parent_message = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True, related_name='replies')  # Support threaded replies
+
 
     def __str__(self):
         return f"From {self.sender} to {self.receiver} at {self.timestamp}"
